fcef167dd659decb2f18172d51b71e336d8785d3
yathaarth2718 <ymjariwala@gmail.com>
2025-03-16 21:48:43 -0600

    Followed a tutorial to finish game, had no idea how to go through with creating infinite levels with random obstacles. But, I did learn a lot from this, so I think following the tutorial was okay.

--- flappy-bird/scripts/bird.gd
+++ flappy-bird/scripts/bird.gd
@@ -1,14 +1,44 @@
 extends CharacterBody2D
 
-const GRAVITY = 10000
-const JUMP_SPEED = 7000
+const GRAVITY = 1000
+const MAX_VEL = 600
+const JUMP_SPEED = -500
+
+var flying :bool = false
+var falling :bool = false
+
+const START_POS = Vector2(100, 400)
+
+func _ready() -> void:
+	reset()
+
+
+func reset() -> void:
+	flying = false
+	falling = false
+	position = START_POS
+	set_rotation(0)
+
+
 
 func _physics_process(delta: float) -> void:
-	velocity.y = GRAVITY * delta
+	if flying or falling:
+		velocity.y += GRAVITY * delta
+		
+		#term vel
+		if velocity.y > MAX_VEL:
+			velocity.y = MAX_VEL
+		if flying:
+			set_rotation(deg_to_rad(velocity.y * 0.05))
+			$AnimatedSprite2D.play()
+		elif falling:
+			set_rotation(PI/2)
+			$AnimatedSprite2D.stop()
+			
+		move_and_collide(velocity * delta)
+	else:
+		$AnimatedSprite2D.stop()
+		
 	
-	if Input.is_action_just_pressed("jump"):
-		velocity.y += -JUMP_SPEED
-	
-	
-	move_and_slide()
-	
+func flap():
+	velocity.y = JUMP_SPEED
